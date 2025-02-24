bce938ebdbcef2256d3ab9e36be38bfa721a28fc
tcwatashi <leandrinho1947@gmail.com>
2025-02-23 22:04:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function selectOption(option) {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
-    } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
+    } else if (option === 'não') {
+        // Change text on the "Não" button to "Tem certeza?"
+        document.getElementById('no-button').innerText = 'Tem certeza?'; 
+        // Increase font size of "Sim" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
         var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
         yesButton.style.fontSize = newSize + 'px';
     } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
+        // If neither "Sim" nor "Não" was clicked, show an alert message
         alert('Invalid option!');
     }
 }
